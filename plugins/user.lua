93587aaebd96dd7e1180a6fc68f4f4ac2592a825
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-06-02 21:06:11 +0800

    add colorscheme and clojure plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,9 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+  "morhetz/gruvbox",
+  { "Olical/conjure", lazy = false },
+  { "gpanders/nvim-parinfer", lazy = false }-- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
